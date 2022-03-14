c63908768d0611ff4bbd9d443c26f61f854ee7d7
Sergio <sergiohscl2@gmail.com>
2022-03-14 19:27:43 -0300

    Criação Tela Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import theme from './constants/theme';
 import { ThemeProvider } from '@material-ui/core/styles';
-
+import LoginPage from './pages/LoginPage/LoginPage'
 
 const  App = () => {
   return (
     <ThemeProvider theme={theme}>
-      Hello World!      
+      Hello World! 
+      <LoginPage/>     
     </ThemeProvider>
   );
 };
---
+++ src/pages/LoginPage/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Img = styled.img`
+    width: 22.5rem;
+    height: 20rem;
+`
+export const LoginForm = styled.form`
+  display: grid;
+  gap: 16px;
+`
+export const Container = styled.div`
+    border: 1px solid #e8222e;
+    width: 22.5rem;
+    height: 44rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+` 
